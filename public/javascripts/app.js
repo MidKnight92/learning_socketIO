782a36f5c127027c1dc8f05bce3742f2be283a6b
Stephanie Viveros <sviveros05@gmail.com>
2020-01-19 00:39:32 -0600

    removed all var variables form scaffold code

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -10,8 +10,8 @@ socket.on('clear-circle', () => {
   circles.innerHTML = '';
 });
 
-var circles = document.getElementById('circles');
-var initials = '';
+let circles = document.getElementById('circles');
+let initials = '';
 
 circles.addEventListener('click', function(evt) {
   socket.emit('add-circle', {
@@ -32,12 +32,12 @@ do {
 } while (initials.length < 2 || initials.length > 3);
 
 function getInitials() {
-  var input = prompt("Please enter your initials");
+  let input = prompt("Please enter your initials");
   return input ? input.toUpperCase() : '';
 }
 
 function addCircle({x, y, dia, rgba, initials}) {
-  var el = document.createElement('div');
+  let el = document.createElement('div');
   el.style.left = x - Math.floor(dia / 2 + 0.5) + 'px';
   el.style.top = y - Math.floor(dia / 2 + 0.5) + 'px';
   el.style.width = el.style.height = dia + 'px';
